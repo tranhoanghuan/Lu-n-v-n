8c07e3eec604518e830252a525d8035f8356976d
phuong vu <phuongtinhbien12@gmail.com>
2018-09-15 12:27:29 +0700

    add PAYMENT.sql, TIME_SCHEDULE.sql

---
+++ Data/sql_query/PAYMENT.sql
@@ -0,0 +1,12 @@
+create table payment (
+    id numeric(19,0) primary key,
+    payment_name varchar(2000),
+    CREATE_BY	NUMERIC(19,0),
+    UPDATE_BY	NUMERIC(19,0),
+    CREATE_DATE	TIMESTAMP,
+    UPDATE_DATE	TIMESTAMP,
+    status varchar(200)
+);
+
+create sequence payment_seq;
+/
---
+++ Data/sql_query/TIME_SCHEDULE.sql
@@ -0,0 +1,13 @@
+create table time_schedule(
+    id numeric(19,0) primary key,
+    time_schedule_no varchar(200),
+    time_start time,
+    time_end time,
+    CREATE_BY	NUMERIC(19,0),
+    UPDATE_BY	NUMERIC(19,0),
+    CREATE_DATE	TIMESTAMP,
+    UPDATE_DATE	TIMESTAMP,
+    status varchar(200)
+);
+create sequence time_schedule_seq;
+/
