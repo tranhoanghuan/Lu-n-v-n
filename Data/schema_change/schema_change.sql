197001f3174a31f9389989a64840079c1299cc6f
phuong vu <phuongtinhbien12@gmail.com>
2018-10-15 23:56:54 +0700

    add banner.svg add table statatics

--- Data/schema_change/schema_change.sql
+++ Data/schema_change/schema_change.sql
@@ -24,3 +24,35 @@ alter table product add column product_avatar INTEGER REFERENCES post(id);
 alter table service_type add column service_type_avatar INTEGER REFERENCES post(id);
 alter table branch add column branch_avatar INTEGER REFERENCES post(id);
 alter table store add column store_avatar INTEGER REFERENCES post(id);
+
+--for statistics 
+
+create table statistics (
+
+    id serial primary key,
+    table_name varchar(4000),
+    code varchar(4000),
+    st_value varchar(4000),
+    st_ext_value varchar(4000),
+    CREATE_DATE	TIMESTAMP default now(),
+    UPDATE_DATE	TIMESTAMP
+);
+
+-- code
+--SERVICE_COUNT
+--CLOTH COUNT
+--PROMOTION_USE_COUNT
+--PROMOTION_VALUES
+--SCHEDULE_COUNT
+--+st_value store time_schedule_no
+--+st_value count used times
+--PRODUCT_USE_COUNT
+--+st_value store id product
+--+st_ext_value count used times
+--+DRAFT_ORDER
+--+SUCCESS_ORDER
+--+
+
+
+
+
